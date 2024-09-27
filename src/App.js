a55290fe16c4fd3725812d6f99ac1cfc19ed9dba
Hugo BERNIER <hugo.bernier88@gmail.com>
2024-09-27 11:45:53 +0200

    Create contactForm

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import DefaultHeader from "./components/defaultHeader";
 import HomeComponent from "./components/homeComponent";
+import ContactForm from "./components/contactForm";
 
 function App() {
   const [section, setSection] = useState("home");
@@ -10,9 +11,7 @@ function App() {
     <div className="min-h-screen bg-background flex flex-col">
       <DefaultHeader setSection={setSection} section={section} />
       <div className="flex-grow flex flex-col items-center">
-        {section === "home" && (
-          <HomeComponent />
-        )}
+        {section === "home" && <HomeComponent />}
         {section === "skills" && (
           <div className="flex flex-col items-center">
             <h1 className="text-6xl text-white">Compétences</h1>
@@ -28,11 +27,7 @@ function App() {
             <h1 className="text-6xl text-white">Formations</h1>
           </div>
         )}
-        {section === "contact" && (
-          <div className="flex flex-col items-center">
-            <h1 className="text-6xl text-white">Contact</h1>
-          </div>
-        )}
+        {section === "contact" && <ContactForm />}
       </div>
     </div>
   );
---
+++ src/components/contactForm.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    company: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Logique de soumission du formulaire ici
+    console.log("Données du formulaire soumises:", formData);
+
+    setFormData({
+      name: "",
+      email: "",
+      company: "",
+      subject: "",
+      message: "",
+    });
+  };
+
+  return (
+    <section>
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white dark:text-white">
+          Contactez-moi
+        </h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text-primary dark:text-gray-400 sm:text-xl">
+          Vous êtes intéressé par mon CV ? N'hésitez pas à me contacter en
+          remplissant le formulaire ci-dessous.
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-8">
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Votre nom
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="Votre nom"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Votre adresse e-mail
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="nom@entreprise.com"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="company"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Nom de l'entreprise
+            </label>
+            <input
+              type="text"
+              id="company"
+              name="company"
+              value={formData.company}
+              onChange={handleChange}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="Nom de l'entreprise"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="subject"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Sujet
+            </label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="Objet de votre message"
+              required
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Votre message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              rows="6"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              placeholder="Expliquez brièvement pourquoi vous me contactez..."
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Envoyer le message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;
